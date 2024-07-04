cece9fa369707371513eee8e8fdd59a91ae21891
anatawa12 <anatawa12@icloud.com>
2024-07-04 09:00:33 +0900

    log from rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Manager, RunEvent};
+
 fn main() {
-  tauri::Builder::default()
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
+    tauri::Builder::default()
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app, event| {
+            if let RunEvent::WindowEvent { event, label, .. } = event {
+                if let tauri::WindowEvent::Resized(size) = event {
+                    let window = app.get_window(&label).expect("failed to get window");
+                    let maximized = window.is_maximized().expect("failed to get maximized");
+                    let fullscreen = window.is_fullscreen().expect("failed to get fullscreen");
+
+                    window
+                        .emit(
+                            "log",
+                            format!(
+                                "Resized to {}x{}\nfullscreen: {}, maximized: {}",
+                                size.width, size.height, fullscreen, maximized
+                            ),
+                        )
+                        .expect("failed to resize");
+                }
+            }
+        });
 }
